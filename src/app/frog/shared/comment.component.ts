5ca9ac2996be254cb81899543f6998c59efbb4ac
Brett Dixon <theiviaxx@gmail.com>
2016-12-04 15:06:15 -0800

    Fixed styles for editing a comment

--- src/app/frog/shared/comment.component.ts
+++ src/app/frog/shared/comment.component.ts
@@ -23,8 +23,8 @@ import { CommentService } from './comment.service';
             <a class="light-green-text">{{comment.user?.name | capitalize:1}}</a>
         </div>
         <div class="commenter-headline grey-text text-darken-1">{{comment.user?.email}}</div>
-        <i *ngIf="isOwner" (click)="edit()" class="material-icons tiny right" [class.light-green-text]="editing">{{(editing) ? "check_circle" : "edit"}}</i>
-        <i *ngIf="editing" (click)="revert()" class="material-icons tiny right red-text">cancel</i>
+        <i *ngIf="isOwner" (click)="edit()" class="material-icons right pointer" [class.light-green-text]="editing">{{(editing) ? "check_circle" : "edit"}}</i>
+        <i *ngIf="editing" (click)="revert()" class="material-icons right red-text pointer">delete_sweep</i>
         
         <div *ngIf="editing">
             <div class="row">
@@ -42,7 +42,7 @@ import { CommentService } from './comment.service';
         </ul>
     </div>`,
     styles: [
-        '.comment-body { position: relative; width: 100%; overflow: visible; display: table-cell; vertical-align: top; line-height: initial; }',
+        '.comment-body { position: relative; width: 100%; overflow: visible; vertical-align: top; line-height: initial; }',
         '.comment-body .commenter a { font-size: 16px; font-weight: 300; }',
         '.comment-body .commenter-headline { font-size: 12px; opacity: 0.6; margin-bottom: 5px; line-height: 1.25em; }',
         '.comment-text { word-break: break-word; }',
@@ -50,7 +50,12 @@ import { CommentService } from './comment.service';
         '.social-actions li { display: inline-block; margin-right: 15px; font-size: 12px; float: right; }',
         'div.commenter a { line-height: inherit; font-size: 20px; padding: 0; height: inherit; }',
         'a { color: #13aff0; transition: all 0.2s cubic-bezier(0.55, 0.085, 0.68, 0.53); }',
-        'p { margin: 0; }'
+        'p { margin: 0; }',
+        'i.pointer { cursor: pointer; }',
+        'textarea { border: none; border-left: 4px solid #4caf50; outline: none; transition: height 500ms; background-color: #1b1b1b; }',
+        'textarea.expanded { height: 100px; }',
+        'textarea::-webkit-input-placeholder { color: #707070; }',
+        '.input-field { margin-top: 2rem; }'
     ]
 })
 export class CommentComponent implements AfterContentInit {
