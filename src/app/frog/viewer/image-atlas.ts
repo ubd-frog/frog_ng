bca392b33686b00a06a753773b4f49e0e4422a5e
Brett Dixon <theiviaxx@gmail.com>
2017-05-22 23:24:56 -0700

    Added ImageAtlas to handle image atlasing and caching

---
+++ src/app/frog/viewer/image-atlas.ts
@@ -0,0 +1,60 @@
+
+
+import {BehaviorSubject, Observable, Subscription} from "rxjs";
+import {CVideo} from "../shared/models";
+
+export const kMaxCanvasSize = 16000;
+
+export class ImageAtlas {
+    private columns: number;
+    private rows: number;
+    private sub: Subscription;
+    public canvas: any;
+    public ctx: CanvasRenderingContext2D;
+    public loadedFrame: BehaviorSubject<number>;
+
+    constructor(private element: HTMLVideoElement, private item: CVideo) {
+        this.canvas = document.createElement('canvas');
+        this.canvas.width = kMaxCanvasSize;
+        this.canvas.height = kMaxCanvasSize;
+        this.ctx = this.canvas.getContext('2d');
+        this.columns = Math.floor(kMaxCanvasSize / this.item.width);
+        this.rows = Math.floor(kMaxCanvasSize / this.item.height);
+        this.loadedFrame = new BehaviorSubject<number>(0);
+
+        this.sub = Observable.fromEvent(<any>this.element, 'seeked').subscribe(event => {
+            let offset = this.getFrame(this.element.currentTime);
+            this.drawFrame(offset);
+        });
+    }
+    getFrame(time: number) {
+        return Math.round(time * this.item.framerate);
+    }
+    getTime(frame: number) {
+        return frame / this.item.framerate;
+    }
+    getCoords(frame: number) {
+        let row = Math.floor(frame / this.columns);
+        let col = Math.round(frame % this.columns);
+
+        return [row, col];
+    }
+    private drawFrame(offset) {
+        this.loadedFrame.next(offset);
+        let coords = this.getCoords(offset);
+
+        this.ctx.drawImage(
+            this.element,
+            coords[1] * this.item.width, coords[0] * this.item.height,
+            this.item.width, this.item.height
+        );
+        let time = this.getTime(offset + 1);
+        if (time <= this.element.duration) {
+            setTimeout(() => this.element.currentTime = time, 0);
+        }
+        else {
+            this.sub.unsubscribe();
+            this.loadedFrame.complete();
+        }
+    }
+}
